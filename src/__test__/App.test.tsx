25bd603c0425e56da16d7567a72d2b479746bffb
Oscar Barrios <oscar.barrios@spreetail.com>
2022-08-22 13:52:12 -0600

    first commit

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,9 @@
+import App from "../App";
+import { render, screen } from "@testing-library/react";
+
+describe("App", () => {
+  it("renders the title", () => {
+    render(<App />);
+    expect(screen.getByText("Hello world!"));
+  });
+});
