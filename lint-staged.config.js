b56149a1202b13afeff0f462159594dfa41d7bc1
Oscar Barrios <oscar.barrios@spreetail.com>
2022-08-22 16:31:31 -0600

    update husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  '*.{ts,tsx,css}': ['prettier . --write'],
   '*.{ts,tsx}': [
     'eslint . --cache --fix --ext .tsx --ext .ts',
-    () => 'npm tsc',
-    () => 'npm jest',
+    () => 'npm run test',
   ],
 }
