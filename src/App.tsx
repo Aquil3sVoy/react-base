1ac41c9dde1c31f3a1987fc17df084845af7505e
Oscar Barrios <oscar.barrios@spreetail.com>
2022-08-22 16:05:41 -0600

    add lint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+function App(): JSX.Element {
+  console.log('App()')
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,9 +1,9 @@
-import App from "../App";
-import { render, screen } from "@testing-library/react";
+import App from '../App'
+import { render, screen } from '@testing-library/react'
 
-describe("App", () => {
-  it("renders the title", () => {
-    render(<App />);
-    expect(screen.getByText("Hello world!"));
-  });
-});
+describe('App', () => {
+  it('renders the title', () => {
+    render(<App />)
+    expect(screen.getByText('Hello world!'))
+  })
+})
