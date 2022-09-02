78c26b98ebaaf97d57c92df964121cd08da03fef
Oscar Barrios <oscar.barrios@spreetail.com>
2022-09-02 13:36:30 -0600

    update dark mode on footer

--- src/shared/components/footer/Footer.tsx
+++ src/shared/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import routes from '../../../view/routes'
 export default function Footer() {
   const footer = routes.routes.footer
   return (
-    <footer className="bg-white">
+    <footer className="bg-white dark:bg-slate-800">
       <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {footer.map((item) => (
